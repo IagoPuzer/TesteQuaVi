a6258cff0a3ceb1e218a4987912e8e865ea646c2
iagop <iagopuzer06@hotmail.com>
2024-04-22 19:54:08 -0300

    Adicionando o prisma ao projeto

---
+++ prisma/migrations/20240422224654_dev/migration.sql
@@ -0,0 +1,11 @@
+-- CreateTable
+CREATE TABLE "notes" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "title" TEXT NOT NULL,
+    "description" TEXT,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "notes_title_key" ON "notes"("title");
