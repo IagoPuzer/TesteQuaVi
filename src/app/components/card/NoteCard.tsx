345503905b3a3556e1ce71a9aebfcec216eb5290
iagop <iagopuzer06@hotmail.com>
2024-04-23 17:17:31 -0300

    Ajustes no noteCard

--- src/app/components/card/NoteCard.tsx
+++ src/app/components/card/NoteCard.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import NoteModal from "../modals/NoteModal";
 import UpdateNoteForm, { NoteData } from "../forms/UpdateNoteForm";
+import ViewNote from "../modals/ViewNote"; // Importe o componente ViewNote aqui
 import { toast } from "sonner";
 
 interface Note {
@@ -16,19 +17,27 @@ interface Props {
 }
 
 export default function NoteCard({ note, onDelete, onUpdate }: Props) {
-  const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
+  const [isUpdateModalOpen, setIsUpdateModalOpen] = useState<boolean>(false);
+  const [isViewModalOpen, setIsViewModalOpen] = useState<boolean>(false); // Novo estado para controlar o modal de visualização
+
   const handleDeleteClick = () => {
     onDelete(note.id);
-    toast.error("Nota excluida");
+    toast.error("Nota excluída");
   };
 
   const handleUpdateClick = () => {
-    setIsModalOpen(true);
+    setIsUpdateModalOpen(true);
   };
 
-  const handleCloseModal = () => {
-    setIsModalOpen(false);
+  const handleViewClick = () => {
+    setIsViewModalOpen(true);
   };
+
+  const handleCloseUpdateModal = () => {
+    setIsUpdateModalOpen(false);
+    setIsViewModalOpen(false);
+  };
+
   return (
     <>
       <div className="overflow-hidden rounded bg-white text-slate-500 shadow-md shadow-slate-200">
@@ -41,30 +50,41 @@ export default function NoteCard({ note, onDelete, onUpdate }: Props) {
           <p className="truncate">{note.description}</p>
           <div className="flex justify-end gap-4 mt-6">
             <button
-              className="bg-red-200 p-2 rounded-md text-black"
+              className="bg-red-200 hover:bg-red-300 p-2 rounded-md text-black"
               onClick={handleDeleteClick}
             >
-              Deletar nota
+              Deletar
             </button>
             <button
-              className="bg-sky-100 p-2 rounded-md text-black"
+              className="bg-sky-100 hover:bg-sky-300 p-2 rounded-md text-black"
               onClick={handleUpdateClick}
             >
-              Editar nota
+              Editar
+            </button>
+            <button
+              className="bg-green-100 hover:bg-green-300 p-2 rounded-md text-black"
+              onClick={handleViewClick}
+            >
+              Visualizar
             </button>
           </div>
         </div>
       </div>
       <NoteModal
-        isOpen={isModalOpen}
+        isOpen={isUpdateModalOpen}
         note={note}
         formComponent={
           <UpdateNoteForm
             initialData={note}
             onSubmit={onUpdate}
-            onClose={handleCloseModal}
+            onClose={handleCloseUpdateModal}
           />
         }
+      />
+      <ViewNote
+        note={note}
+        isOpen={isViewModalOpen}
+        onClose={handleCloseUpdateModal}
       />
     </>
   );
